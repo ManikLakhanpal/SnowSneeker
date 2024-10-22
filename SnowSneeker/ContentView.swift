bbd3187d876bfe91a6aaea835ab80529b7cbf8d0
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-10-22 10:15:42 +0530

    Learned 'NavigationSplitView' and usecases

--- SnowSneeker/ContentView.swift
+++ SnowSneeker/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView(preferredCompactColumn: .constant(.detail)) {
+            NavigationLink("Primary") {
+                Text("New view")
+            }
+            
+            NavigationLink("Manik") {
+                Text("Lakhanpal")
+            }
+        } detail: {
+            Text("Content")
+                .navigationTitle("Content View")
         }
-        .padding()
+        .navigationSplitViewStyle(.balanced)
     }
 }
 
