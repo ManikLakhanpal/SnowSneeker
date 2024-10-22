5abcf4b013582fe11380ce42bf5976e231609867
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-10-22 09:29:35 +0530

    Initial Commit

---
+++ SnowSneeker/SnowSneekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSneekerApp.swift
+//  SnowSneeker
+//
+//  Created by Manik Lakhanpal on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSneekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowSneekerTests/SnowSneekerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSneekerTests.swift
+//  SnowSneekerTests
+//
+//  Created by Manik Lakhanpal on 22/10/24.
+//
+
+import Testing
+@testable import SnowSneeker
+
+struct SnowSneekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
